930fdc91525507a3173c0da08645c76ad84030aa
Volodymyr Voloshyn <mailvoloshyn@gmail.com>
2025-04-07 22:10:45 +0300

    Add lesson6 tasks

---
+++ functions/extraTask3.js
@@ -0,0 +1,12 @@
+function countDiscount(days){
+	if (days <= 0) return "Unacceptable value"
+	const oneDayPrice = 40
+	if (days >= 7) return oneDayPrice * days - 50
+	if (days >= 3) return oneDayPrice * days - 20
+	return oneDayPrice * days
+}
+
+console.log(countDiscount(0));
+console.log(countDiscount(10));
+console.log(countDiscount(5));
+console.log(countDiscount(2));
