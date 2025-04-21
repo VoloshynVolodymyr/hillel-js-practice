761a7600538174a91a01c05304cd95eba662b486
Volodymyr Voloshyn <mailvoloshyn@gmail.com>
2025-04-21 22:54:24 +0300

    add tasks with OOP

---
+++ OOP/Book.js
@@ -0,0 +1,50 @@
+export default class Book {
+	_title
+	_author
+	_year
+
+	constructor(title, author, year){
+		this.title = title
+		this.author = author
+		this.year = year
+	}
+
+	static sortBooksByYear(arr){
+		const sortedBooks = arr.sort((a, b) => a.year - b.year)
+		return sortedBooks[0].printInfo()
+	}
+
+	get title() {
+		return this._title
+	}
+	set title(value) {
+		if(typeof value !== "string"){
+			throw new Error("The title must be a string!")
+		}
+		this._title = value
+	}
+
+	get author() {
+		return this._author
+	}
+	set author(value) {
+		if(typeof value !== "string"){
+			throw new Error("The author must be a string!")
+		}
+		this._author = value
+	}
+
+	get year() {
+		return this._year
+	}
+	set year(value) {
+		if(typeof value !== "number"){
+			throw new Error("The year must be a number!")
+		}
+		this._year = value
+	}
+
+	printInfo(){
+		console.log(`Book : ${this.title}, author : ${this.author}, year : ${this.year}`);
+	}
+}
---
+++ OOP/EBook.js
@@ -0,0 +1,28 @@
+import Book from './Book.js'
+
+export default class EBook extends Book {
+	#format
+
+	constructor(title, author, year, format){
+		super(title, author, year)
+		this.format = format
+	}
+
+	get format() {
+		return this.#format
+	}
+	set format(value) {
+		if (typeof value !== "string") {
+			throw new Error("Format must be a string!")
+		}
+		this.#format = value
+	}
+
+	static createInstanceFromBook(book, format){
+		return new EBook(book.title, book.author, book.year, format)
+	}
+	printInfo(){
+		super.printInfo()
+		console.log(`Format : ${this.format}`)
+	}
+}
